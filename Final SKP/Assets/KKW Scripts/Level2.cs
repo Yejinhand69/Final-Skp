0c4a5034f536a07b1e8d674230000665032331ae
Kon Kok Wei <0134820@student.uow.edu.my>
2024-01-27 15:56:30 +0800

    changes

--- Final SKP/Assets/KKW Scripts/Level2.cs
+++ Final SKP/Assets/KKW Scripts/Level2.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level2 : MonoBehaviour
 {
+
+    public GameObject pic1;
+    public GameObject pic2;
+    public GameObject pic3;
+    public GameObject pic4;
+    public GameObject pic5;
+
+    LevelSelection levelSelection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Prephase());
+        Debug.Log(levelSelection.lvl2completed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void Option1()
+    {
+        //StartCoroutine(Phase1());
+        levelSelection.lvl2completed = true;
+        Debug.Log(levelSelection.lvl2completed);
+        SceneManager.LoadScene("LevelSelection");
+    }
+    public void Option2()
+    {
+        StartCoroutine(Phase2());
+    }
+    public void Option3()
+    {
+        StartCoroutine(Phase3());
+    }
+
+
+
+    IEnumerator Prephase()
+    {
+        yield return new WaitForSeconds(5);
+        pic1.SetActive(false);
+        pic2.SetActive(false);
+        pic3.SetActive(true);
+        yield return new WaitForSeconds(5);
+        pic5.SetActive(true);
+    }
+
+    IEnumerator Phase1()
+    {
+        yield return new WaitForSeconds(5);
+        pic1.SetActive(false);
+        pic2.SetActive(false);
+        pic3.SetActive(true);
+        yield return new WaitForSeconds(5);
+        pic5.SetActive(true);
+    }
+
+    IEnumerator Phase2()
+    {
+        yield return new WaitForSeconds(5);
+        pic1.SetActive(false);
+        pic2.SetActive(false);
+        pic3.SetActive(true);
+        yield return new WaitForSeconds(5);
+        pic5.SetActive(true);
+    }
+
+    IEnumerator Phase3()
+    {
+        yield return new WaitForSeconds(5);
+        pic1.SetActive(false);
+        pic2.SetActive(false);
+        pic3.SetActive(true);
+        yield return new WaitForSeconds(5);
+        pic5.SetActive(true);
+    }
+
 }
--- Final SKP/Assets/KKW Scripts/LevelSelection.cs
+++ Final SKP/Assets/KKW Scripts/LevelSelection.cs
@@ -18,6 +18,18 @@ public class LevelSelection : MonoBehaviour
     public GameObject Level9;
     public GameObject Level10;
 
+    public GameObject Level1Completed;
+    public GameObject Level2Completed;
+    public GameObject Level3Completed;
+    public GameObject Level4Completed;
+    public GameObject Level5Completed;
+
+    public GameObject Level6Completed;
+    public GameObject Level7Completed;
+    public GameObject Level8Completed;
+    public GameObject Level9Completed;
+    public GameObject Level10Completed;
+
     public GameObject page1;
     public GameObject page2;
     public GameObject page3;
@@ -26,6 +38,16 @@ public class LevelSelection : MonoBehaviour
     public GameObject backButton;
     public GameObject nextButton;
 
+    public bool lvl1completed = false;
+    public bool lvl2completed = false;
+    public bool lvl3completed = false;
+    public bool lvl4completed = false;
+    public bool lvl5completed = false;
+    public bool lvl6completed = false;
+    public bool lvl7completed = false;
+    public bool lvl8completed = false;
+    public bool lvl9completed = false;
+    public bool lvl10completed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -37,19 +59,59 @@ public class LevelSelection : MonoBehaviour
     {
         if (pageNum == 1) 
         {
-            Level1.SetActive(true);
-            Level2.SetActive(true);
-            Level3.SetActive(true);
+            if (!lvl1completed)
+            {
+                Level1.SetActive(true);
+                Level1Completed.SetActive(false);
+            }
+            else if (lvl1completed)
+            {
+                Level1.SetActive(false);
+                Level1Completed.SetActive(true);
+            }
+
+            if (!lvl2completed)
+            {
+                Level2.SetActive(true);
+                Level2Completed.SetActive(false);
+            }
+            else if (lvl2completed)
+            {
+                Level2.SetActive(false);
+                Level2Completed.SetActive(true);
+            }
+
+            if (!lvl3completed)
+            {
+                Level3.SetActive(true);
+                Level3Completed.SetActive(false);
+            }
+            else if (lvl3completed)
+            {
+                Level3.SetActive(false);
+                Level3Completed.SetActive(true);
+            }
 
             Level4.SetActive(false);
+            Level4Completed.SetActive(false);
+
             Level5.SetActive(false);
+            Level5Completed.SetActive(false);
+
             Level6.SetActive(false);
+            Level6Completed.SetActive(false);
 
             Level7.SetActive(false);
+            Level7Completed.SetActive(false);
+
             Level8.SetActive(false);
+            Level8Completed.SetActive(false);
+
             Level9.SetActive(false);
+            Level9Completed.SetActive(false);
 
             Level10.SetActive(false);
+            Level10Completed.SetActive(false);
 
             backButton.SetActive(false);
             nextButton.SetActive(true);
@@ -62,19 +124,59 @@ public class LevelSelection : MonoBehaviour
         }
         else if (pageNum == 2)
         {
-            Level1.SetActive(false);
-            Level2.SetActive(false);
-            Level3.SetActive(false);
+            if (!lvl4completed)
+            {
+                Level4.SetActive(true);
+                Level4Completed.SetActive(false);
+            }
+            else if (lvl4completed)
+            {
+                Level4.SetActive(false);
+                Level4Completed.SetActive(true);
+            }
 
-            Level4.SetActive(true);
-            Level5.SetActive(true);
-            Level6.SetActive(true);
+            if (!lvl5completed)
+            {
+                Level5.SetActive(true);
+                Level5Completed.SetActive(false);
+            }
+            else if (lvl5completed)
+            {
+                Level5.SetActive(false);
+                Level5Completed.SetActive(true);
+            }
+
+            if (!lvl6completed)
+            {
+                Level6.SetActive(true);
+                Level6Completed.SetActive(false);
+            }
+            else if (lvl6completed)
+            {
+                Level6.SetActive(false);
+                Level6Completed.SetActive(true);
+            }
+
+            Level1.SetActive(false);
+            Level1Completed.SetActive(false);
+
+            Level2.SetActive(false);
+            Level2Completed.SetActive(false);
+
+            Level3.SetActive(false);
+            Level3Completed.SetActive(false);
 
             Level7.SetActive(false);
+            Level7Completed.SetActive(false);
+
             Level8.SetActive(false);
+            Level8Completed.SetActive(false);
+
             Level9.SetActive(false);
+            Level9Completed.SetActive(false);
 
             Level10.SetActive(false);
+            Level10Completed.SetActive(false);
 
             backButton.SetActive(true);
             nextButton.SetActive(true);
@@ -86,19 +188,59 @@ public class LevelSelection : MonoBehaviour
         }
         else if (pageNum == 3)
         {
+            if (!lvl7completed)
+            {
+                Level7.SetActive(true);
+                Level7Completed.SetActive(false);
+            }
+            else if (lvl7completed)
+            {
+                Level7.SetActive(false);
+                Level7Completed.SetActive(true);
+            }
+
+            if (!lvl8completed)
+            {
+                Level8.SetActive(true);
+                Level8Completed.SetActive(false);
+            }
+            else if (lvl8completed)
+            {
+                Level8.SetActive(false);
+                Level8Completed.SetActive(true);
+            }
+
+            if (!lvl9completed)
+            {
+                Level9.SetActive(true);
+                Level9Completed.SetActive(false);
+            }
+            else if (lvl9completed)
+            {
+                Level9.SetActive(false);
+                Level9Completed.SetActive(true);
+            }
+
             Level1.SetActive(false);
+            Level1Completed.SetActive(false);
+
             Level2.SetActive(false);
+            Level2Completed.SetActive(false);
+
             Level3.SetActive(false);
+            Level3Completed.SetActive(false);
 
             Level4.SetActive(false);
-            Level5.SetActive(false);
-            Level6.SetActive(false);
+            Level4Completed.SetActive(false);
 
-            Level7.SetActive(true);
-            Level8.SetActive(true);
-            Level9.SetActive(true);
+            Level5.SetActive(false);
+            Level5Completed.SetActive(false);
+
+            Level6.SetActive(false);
+            Level6Completed.SetActive(false);
 
             Level10.SetActive(false);
+            Level10Completed.SetActive(false);
 
             backButton.SetActive(true);
             nextButton.SetActive(true);
@@ -110,19 +252,45 @@ public class LevelSelection : MonoBehaviour
         }
         else if (pageNum == 4)
         {
+
+            if (!lvl10completed)
+            {
+                Level10.SetActive(true);
+                Level10Completed.SetActive(false);
+            }
+            else if (lvl10completed)
+            {
+                Level10.SetActive(false);
+                Level10Completed.SetActive(true);
+            }
             Level1.SetActive(false);
+            Level1Completed.SetActive(false);
+
             Level2.SetActive(false);
+            Level2Completed.SetActive(false);
+
             Level3.SetActive(false);
+            Level3Completed.SetActive(false);
 
             Level4.SetActive(false);
+            Level4Completed.SetActive(false);
+
             Level5.SetActive(false);
+            Level5Completed.SetActive(false);
+
             Level6.SetActive(false);
+            Level6Completed.SetActive(false);
 
             Level7.SetActive(false);
-            Level8.SetActive(false);
-            Level9.SetActive(false);
+            Level7Completed.SetActive(false);
 
-            Level10.SetActive(true);
+            Level8.SetActive(false);
+            Level8Completed.SetActive(false);
+
+            Level9.SetActive(false);
+            Level9Completed.SetActive(false);
+
+
 
             backButton.SetActive(true);
             nextButton.SetActive(false);
@@ -148,14 +316,14 @@ public class LevelSelection : MonoBehaviour
 
     public void Level1Select()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Level 1");
     }
     public void Level2Select()
     {
-        SceneManager.LoadScene("Level2");
+        SceneManager.LoadScene("Level 2");
     }
     public void Level3Select()
     {
-        SceneManager.LoadScene("Level3");
+        SceneManager.LoadScene("Level 3");
     }
 }
